fbf73870e02baa121338b1371029bda1d0083fe8
Chensn <onlottlr@gmail.com>
2025-03-19 21:40:45 +0800

    TEST UPDATE

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ let headerH1 = document.querySelector("header div.logo h1");
 let headerAnchor = document.querySelectorAll("header nav ul li a");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset != 0) {
-    header.style = "background-color:rgba(1, 89, 88,0.5);";
-    headerH1.style = "color:white;";
-    headerAnchor.forEach((a) => {
-      a.style = "color:white;";
-    });
-  } else {
-    header.style = "";
-    headerH1.style = "";
-    headerAnchor.forEach((a) => {
-      a.style = "";
-    });
-  }
+    if (window.pageYOffset != 0) {
+        header.style = "background-color:rgba(1, 89, 88,0.5);";
+        headerH1.style = "color:white;";
+        headerAnchor.forEach(a => {
+            a.style = "color:white;";
+        });
+    } else {
+        header.style = "";
+        headerH1.style = "";
+        headerAnchor.forEach(a => {
+            a.style = "";
+        });
+    }
 });
